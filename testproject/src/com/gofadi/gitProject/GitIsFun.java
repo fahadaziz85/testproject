f03fd7670e532a919d9f3e1baf8a6481637d3e07
fahadaziz <fahadaziz@fahads-mbp>
2021-01-16 13:18:55 +0100

    First Initial commit by GOFadi

---
+++ testproject/src/com/gofadi/gitProject/GitIsFun.java
@@ -0,0 +1,11 @@
+package com.gofadi.gitProject;
+
+public class GitIsFun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("GIT is Fun");
+
+	}
+
+}
