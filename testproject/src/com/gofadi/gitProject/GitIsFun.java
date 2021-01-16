2c5894e9decda19d013998edf8c1ee1af9e83f67
fahadaziz <fahadaziz@fahads-mbp>
2021-01-16 13:36:25 +0100

    Second Class TestAutomation commit

--- testproject/src/com/gofadi/gitProject/GitIsFun.java
+++ testproject/src/com/gofadi/gitProject/GitIsFun.java
@@ -1,10 +1,18 @@
 package com.gofadi.gitProject;
 
+import com.gofadi.testautomation.TestAutomation;
+
 public class GitIsFun {
 
+	TestAutomation test = new TestAutomation();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("GIT is Fun");
+		
+		TestAutomation test = new TestAutomation();
+		System.out.println(test.getTestAutomation());
+		
+		
 
 	}
 
